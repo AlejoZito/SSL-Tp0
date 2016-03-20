43022e24df866693b5432022efc9480a072992e3
AlejoZito <zitoalejo@gmail.com>
2016-03-20 17:50:19 -0300

    upload 0

---
+++ main.c
@@ -0,0 +1,12 @@
+//
+//  SSL - Tp0 - Hola mundo C Standard
+//  Alejo Zito - Legajo 1562629
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hola mundo\n");
+    return 0;
+}
